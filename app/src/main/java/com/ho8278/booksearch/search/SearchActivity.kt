c458320f5ea2fdc1156b575d199261017ec63bc5
HyoungWoong <yhw112@gmail.com>
2023-11-12 12:12:51 +0900

    텍스트 검색 기능 추가

--- app/src/main/java/com/ho8278/booksearch/search/SearchActivity.kt
+++ app/src/main/java/com/ho8278/booksearch/search/SearchActivity.kt
@@ -1,11 +1,20 @@
 package com.ho8278.booksearch.search
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.EditText
 import androidx.activity.viewModels
+import androidx.core.widget.doOnTextChanged
 import com.ho8278.booksearch.databinding.ActivitySearchBinding
+import com.ho8278.lib.flowbinding.textChanges
 import com.ho8278.lib.lifecycle.activity.LifecycleActivity
 import com.ho8278.lib.lifecycle.activity.untilLifecycle
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
@@ -18,6 +27,18 @@ class SearchActivity : LifecycleActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        initEdiText()
+        initRecyclerView()
+    }
+
+    private fun initEdiText() {
+        binding.search.textChanges()
+            .debounce(500L)
+            .onEach { viewModel.loadFirst(it) }
+            .untilLifecycle(this)
+    }
+
+    private fun initRecyclerView() {
         binding.recyclerView.adapter = booksAdapter
 
         viewModel.booksList
@@ -25,7 +46,5 @@ class SearchActivity : LifecycleActivity() {
                 booksAdapter.submitList(it)
             }
             .untilLifecycle(this)
-
-        viewModel.loadFirst("android")
     }
 }
